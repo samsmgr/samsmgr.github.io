7a4369ba59ea17c63f752ffff16e4246ed8181d7
samsmgr <52979626+samsmgr@users.noreply.github.com>
2019-08-28 19:16:23 -0400

    Add files via upload
    remake of website

---
+++ pw.js
@@ -0,0 +1,19 @@
+function goSunset1() {
+  window.location.href = "sunset1.png";
+
+}
+
+function goSunset2() {
+  window.location.href = "sunset2.png";
+
+}
+
+function goMaverick() {
+  window.location.href = "mavericktree.png";
+
+}
+
+function goFirepit() {
+  window.location.href = "firepit.png";
+
+}
